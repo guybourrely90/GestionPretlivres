d59c4a7f84d186dbeb0e54545ee0df39943f07dc
gby <guybourrely90@gmail.com>
2018-09-15 17:45:00 +0200

    Représentation du module par une variable: js/app.js / création du contrôleur: js/controller.js / Déclaration du fichier controller.js dans la SPA - index.html

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+// Module sous forme d'une variable
+var app = angular.module('starter', ['ionic']);
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs).
